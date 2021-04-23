314ff6290f110b617856561462eaf727206136b3
Adrian Vatchinsky <adrian@getuni.io>
2021-04-22 22:42:04 -0400

    prog

--- ios/Plugin/CAPFirebaseAuth.swift
+++ ios/Plugin/CAPFirebaseAuth.swift
@@ -1,7 +1,16 @@
 import Foundation
 
 @objc public class CAPFirebaseAuth: NSObject {
-    @objc public func echo(_ value: String) -> String {
-        return value
+    
+    @objc public func signIn(providerId: String) -> String {
+        print("signIn", providerId)
+        
+        return providerId
+    }
+    
+    @objc public func signOut() -> Bool  {
+        print("signOut")
+        
+        return true
     }
 }
--- ios/Plugin/CAPFirebaseAuthPlugin.swift
+++ ios/Plugin/CAPFirebaseAuthPlugin.swift
@@ -1,6 +1,8 @@
 import Foundation
 import Capacitor
 
+import Firebase
+
 /**
  * Please read the Capacitor iOS Plugin Development Guide
  * here: https://capacitorjs.com/docs/plugins/ios
@@ -8,11 +10,36 @@ import Capacitor
 @objc(CAPFirebaseAuthPlugin)
 public class CAPFirebaseAuthPlugin: CAPPlugin {
     private let implementation = CAPFirebaseAuth()
+    
+    public override func load(){
+        
+        print("LOADDDDDD")
+        
+        if (FirebaseApp.app() == nil) {
+           FirebaseApp.configure()
+       }
+    }
 
-    @objc func echo(_ call: CAPPluginCall) {
-        let value = call.getString("value") ?? ""
+    
+    @objc func signIn(_ call: CAPPluginCall) {
+        guard let providerId = call.options["providerId"] as? String else {
+           call.reject("providerId must be provided")
+           return
+        }
+        
+        print("!!!!!!!!!!!!!")
+        print("SignIn", providerId)
+        
+
         call.resolve([
-            "value": implementation.echo(value)
+            "value": implementation.signIn(providerId: providerId)
+        ])
+    }
+    
+    @objc func signOut(_ call: CAPPluginCall) {
+        
+        call.resolve([
+            "value": implementation.signOut()
         ])
     }
 }
